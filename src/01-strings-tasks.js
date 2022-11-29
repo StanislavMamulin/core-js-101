58f4242171f473db044790db064f0eb9ee7ea301
Stanislav Mamulin <StanislavPM@ya.ru>
2022-11-29 19:19:51 +0500

    feat: implement function returns true if the value is string; otherwise false.

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -282,10 +282,17 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
-}
+function isString(value) {
+  if (
+    value != null
+    && (typeof value === 'string'
+    || typeof value.charAt === 'function')
+  ) {
+    return true;
+  }
 
+  return false;
+}
 
 /**
  * Returns playid card id.

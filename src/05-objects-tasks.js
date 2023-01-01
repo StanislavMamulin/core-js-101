426cf154a36148b9341e577486a849a45e6672af
Stanislav Mamulin <StanislavPM@ya.ru>
2023-01-01 21:13:03 +0500

    feat: Returns the object of specified type from JSON representation

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -55,10 +55,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const propObj = JSON.parse(json);
+  const newObj = Object.create(proto);
+  return Object.assign(newObj, propObj);
 }
-
 
 /**
  * Css selectors builder
